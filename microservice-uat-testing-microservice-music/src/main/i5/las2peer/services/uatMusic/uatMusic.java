d727dab4fbf8100e5c73e710b1effe7b3cb72e53
Melisa Cecilia <melisa.cecilia@yahoo.com>
2017-11-19 16:44:12 +0100

    Update uatMusic.java

--- microservice-uat-testing-microservice-music/src/main/i5/las2peer/services/uatMusic/uatMusic.java
+++ microservice-uat-testing-microservice-music/src/main/i5/las2peer/services/uatMusic/uatMusic.java
@@ -131,7 +131,7 @@ public class uatMusic extends RESTService {
 
     try {
       Object returnServicePostImage = Context.getCurrent().invoke(
-          "uatTestImage", "postImage", postImageParameter);
+          "i5.las2peer.services.uatTestImage.uatTestImage", "postImage", postImageParameter);
       int imageId = (int) returnServicePostImage;
 
       // now process music object
@@ -179,7 +179,7 @@ public class uatMusic extends RESTService {
 
     try {
       Object returnServiceGetImage = Context.getCurrent().invoke(
-          "uatTestImage", "getImage");
+          "i5.las2peer.services.uatTestImage.uatTestImage", "getImage");
       HashMap<Integer, classes.image> imageMap = new HashMap<Integer, classes.image>();
       // put into array
       JSONParser parser = new JSONParser();
